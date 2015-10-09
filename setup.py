766768f0adf8ee24090a6afcfb8c8ad51fbe13fa
luca.rebuffi@elettra.eu <Bacilotto_00>
2015-10-08 23:05:07 -0500

    Issues #19, #21, #24

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import subprocess
 from setuptools import find_packages, setup
 
 NAME = 'ShadowOui'
-VERSION = '1.2.11'
+VERSION = '1.2.12'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
