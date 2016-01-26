b5e649621b1dadb3682042ac6b83f3eabe72b0cc
Luca Rebuffi <luca.rebuffi@elettra.eu>
2016-01-26 13:03:26 +0100

    New Dabam Functionalities for Lenght generation

--- orangecontrib/shadow/widgets/preprocessor/dabam_height_profile.py
+++ orangecontrib/shadow/widgets/preprocessor/dabam_height_profile.py
@@ -68,6 +68,11 @@ class OWdabam_height_profile(OWWidget):
 
     step_x = Setting(1.0)
     dimension_x = Setting(10.0)
+
+    modify_y = Setting(0)
+    new_length = Setting(200.0)
+    filler_value = Setting(0.0)
+
     scale_factor_y = Setting(1.0)
     renormalize_y = Setting(1)
     error_type_y = Setting(0)
@@ -195,15 +200,26 @@ class OWdabam_height_profile(OWWidget):
         self.scrollarea.setWidget(self.table)
         self.scrollarea.setWidgetResizable(1)
 
-        output_profile_box = oasysgui.widgetBox(tab_gener, "Surface Generation Parameters", addSpace=True, orientation="vertical", height=190)
+        output_profile_box = oasysgui.widgetBox(tab_gener, "Surface Generation Parameters", addSpace=True, orientation="vertical", height=250)
 
         self.le_dimension_x = oasysgui.lineEdit(output_profile_box, self, "dimension_x", "Width",
                            labelWidth=300, valueType=float, orientation="horizontal")
         self.le_step_x = oasysgui.lineEdit(output_profile_box, self, "step_x", "Step Width",
                            labelWidth=300, valueType=float, orientation="horizontal")
 
-        oasysgui.lineEdit(output_profile_box, self, "scale_factor_y", "Scale Factor for Length",
-                           labelWidth=300, valueType=float, orientation="horizontal")
+        gui.comboBox(output_profile_box, self, "modify_y", label="Modify Length?", labelWidth=300,
+                     items=["No", "Rescale to new length", "Fit to new length (fill or cut)"], callback=self.set_ModifyY, sendSelectedValue=False, orientation="horizontal")
+
+        self.modify_box_1 = oasysgui.widgetBox(output_profile_box, "", addSpace=False, orientation="vertical", height=50)
+
+        self.modify_box_2 = oasysgui.widgetBox(output_profile_box, "", addSpace=False, orientation="vertical", height=50)
+        oasysgui.lineEdit(self.modify_box_2, self, "scale_factor_y", "Scale Factor", labelWidth=300, valueType=float, orientation="horizontal")
+
+        self.modify_box_3 = oasysgui.widgetBox(output_profile_box, "", addSpace=False, orientation="vertical", height=50)
+        self.le_new_length = oasysgui.lineEdit(self.modify_box_3, self, "new_length", "New Length", labelWidth=300, valueType=float, orientation="horizontal")
+        oasysgui.lineEdit(self.modify_box_3, self, "filler_value", "Filler Value (if new length > profile length) [nm]", labelWidth=300, valueType=float, orientation="horizontal")
+
+        self.set_ModifyY()
 
         gui.comboBox(output_profile_box, self, "renormalize_y", label="Renormalize Length Profile to different RMS", labelWidth=300,
                      items=["No", "Yes"], callback=self.set_RenormalizeY, sendSelectedValue=False, orientation="horizontal")
@@ -259,7 +275,8 @@ class OWdabam_height_profile(OWWidget):
         label.setText(label.text() + " [" + self.workspace_units_label + "]")
         label = self.le_step_x.parent().layout().itemAt(0).widget()
         label.setText(label.text() + " [" + self.workspace_units_label + "]")
-
+        label = self.le_new_length.parent().layout().itemAt(0).widget()
+        label.setText(label.text() + " [" + self.workspace_units_label + "]")
 
     def initializeTabs(self):
         self.tabs = gui.tabWidget(self.mainArea)
@@ -354,6 +371,11 @@ class OWdabam_height_profile(OWWidget):
         self.plot_canvas[plot_canvas_index].setGraphXLabel(xtitle)
         self.plot_canvas[plot_canvas_index].setGraphYLabel(ytitle)
         self.plot_canvas[plot_canvas_index].replot()
+
+    def set_ModifyY(self):
+        self.modify_box_1.setVisible(self.modify_y == 0)
+        self.modify_box_2.setVisible(self.modify_y == 1)
+        self.modify_box_3.setVisible(self.modify_y == 2)
 
     def set_RenormalizeY(self):
         self.output_profile_box_1.setVisible(self.renormalize_y==1)
@@ -467,9 +489,76 @@ class OWdabam_height_profile(OWWidget):
 
             combination = "EF"
 
-            profile_1D_y_x = self.si_to_user_units * self.server.y * self.scale_factor_y
-            if self.use_undetrended == 0: profile_1D_y_y = self.si_to_user_units * self.server.zHeights
-            else: profile_1D_y_y = self.si_to_user_units * self.server.zHeightsUndetrended
+            if self.modify_y == 2:
+                profile_1D_y_x_temp = self.si_to_user_units * self.server.y
+                if self.use_undetrended == 0: profile_1D_y_y_temp = self.si_to_user_units * self.server.zHeights
+                else: profile_1D_y_y_temp = self.si_to_user_units * self.server.zHeightsUndetrended
+
+                first_coord = profile_1D_y_x_temp[0]
+                second_coord  = profile_1D_y_x_temp[1]
+                last_coord = profile_1D_y_x_temp[-1]
+                step = numpy.abs(second_coord - first_coord)
+                length = numpy.abs(last_coord - first_coord)
+                n_points_old = len(profile_1D_y_x_temp)
+
+                if self.new_length > length:
+                    difference = self.new_length - length
+
+                    n_added_points = int(difference/step)
+                    if difference % step == 0:
+                        n_added_points += 1
+                    if n_added_points % 2 != 0:
+                        n_added_points += 1
+
+
+                    profile_1D_y_x = numpy.arange(n_added_points + n_points_old) * step
+                    profile_1D_y_y = numpy.ones(n_added_points + n_points_old) * self.filler_value * 1e-9 * self.si_to_user_units
+                    profile_1D_y_y[int(n_added_points/2) : n_points_old + int(n_added_points/2)] = profile_1D_y_y_temp
+
+                    '''
+                    profile_1D_y_x = numpy.zeros(n_added_points + n_points_old)
+                    profile_1D_y_y = numpy.ones(n_added_points + n_points_old) * self.filler_value * 1e-9 * self.si_to_user_units
+
+                    for index in range(0, int(n_added_points/2)):
+                        profile_1D_y_x[index] = index * step
+
+                    for index in range(int(n_added_points/2), n_points_old + int(n_added_points/2)):
+                        profile_1D_y_x[index] = index * step
+                        profile_1D_y_y[index] = profile_1D_y_y_temp[index - int(n_added_points/2)]
+
+                    for index in range(n_points_old + int(n_added_points/2), n_points_old + n_added_points):
+                        profile_1D_y_x[index] = index * step
+                    '''
+                elif self.new_length < length:
+                    difference = length - self.new_length
+
+                    n_removed_points = int(difference/step)
+                    if difference % step == 0:
+                        n_removed_points -= 1
+                    if n_removed_points % 2 != 0:
+                        n_removed_points -= 1
+
+                    if n_removed_points >= 2:
+                        profile_1D_y_x = profile_1D_y_x_temp[0 : (n_points_old - n_removed_points)]
+                        profile_1D_y_y = profile_1D_y_y_temp[(int(n_removed_points/2) - 1) : (n_points_old - int(n_removed_points/2) - 1)]
+
+                    else:
+                        profile_1D_y_x = profile_1D_y_x_temp
+                        profile_1D_y_y = profile_1D_y_y_temp
+                else:
+                    profile_1D_y_x = profile_1D_y_x_temp
+                    profile_1D_y_y = profile_1D_y_y_temp
+
+            else:
+                if self.modify_y == 0:
+                    profile_1D_y_x = self.si_to_user_units * self.server.y
+                elif self.modify_y == 1:
+                    profile_1D_y_x = self.si_to_user_units * self.server.y * self.scale_factor_y
+
+                if self.use_undetrended == 0: profile_1D_y_y = self.si_to_user_units * self.server.zHeights
+                else: profile_1D_y_y = self.si_to_user_units * self.server.zHeightsUndetrended
+
+
 
             if self.renormalize_y == 0:
                 rms_y = None
@@ -563,6 +652,11 @@ class OWdabam_height_profile(OWWidget):
     def check_fields(self):
         self.dimension_x = congruence.checkStrictlyPositiveNumber(self.dimension_x, "Dimension X")
         self.step_x = congruence.checkStrictlyPositiveNumber(self.step_x, "Step X")
+        if self.modify_y == 1:
+            self.scale_factor_y = congruence.checkStrictlyPositiveNumber(self.scale_factor_y, "Scale Factor")
+        elif self.modify_y == 2:
+            self.new_length = congruence.checkStrictlyPositiveNumber(self.new_length, "New Length")
+
         if self.renormalize_y == 1:
             self.rms_y = congruence.checkPositiveNumber(self.rms_y, "Rms Y")
 
