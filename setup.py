ace9da0e0a66a1e566435ba1a5e56d06b549242b
Luca Rebuffi <luca.rebuffi@elettra.eu>
2016-02-08 10:31:36 +0100

    Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 NAME = 'ShadowOui'
-VERSION = '1.3.7'
+VERSION = '1.3.8'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
