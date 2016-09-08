2c44c2c068e7c50271dd52afff84fc037c7f6145
Luca Rebuffi <luca.rebuffi@elettra.eu>
2016-09-08 20:34:40 +0200

    UM management improved on OASYS

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 NAME = 'ShadowOui'
-VERSION = '1.3.48'
+VERSION = '1.3.49'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -41,9 +41,9 @@ INSTALL_REQUIRES = (
     'numpy',
     'scipy>=0.17.0',
     'matplotlib==1.4.3', #problems found with 1.5.0
-    'srxraylib>=1.0.3',
+    'srxraylib>=1.0.6',
     'orange-widget-core>=0.0.2',
-    'oasys>=0.1.18',
+    'oasys>=0.1.19',
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
