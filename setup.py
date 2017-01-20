9c3aea66e4572dd0587a7e0f9894c69f980d1c6f
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-01-20 10:03:06 +0100

    Hybrid Enhancement (COMPLETED)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import find_packages, setup
 
 NAME = 'ShadowOui'
-VERSION = '1.3.96'
+VERSION = '1.3.97'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
