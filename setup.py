54aa622a1cf7544b71e7730a27bc3f3bfe6d71fe
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-06-03 13:38:22 +0200

    to PyQt5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.4.8'
+VERSION = '1.4.9'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
