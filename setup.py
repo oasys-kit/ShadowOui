14f2ba4703df63ab0f782fa6ab87bb892436b4f1
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-06-09 12:28:01 +0200

    Bug fixing and Issues

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.4.13'
+VERSION = '1.4.14'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.0.0',
+    'oasys1>=1.0.11',
     'shadow3'
 )
 
