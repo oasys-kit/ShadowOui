441376345cc4d65e7de7d0717dbd26747c628ad3
luca.rebuffi@elettra.eu <Bacilotto_00>
2018-04-16 12:17:11 +0200

    RAFRY SHADOW3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.4.45'
+VERSION = '1.4.46'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -40,9 +40,9 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'oasys1>=1.1.3',
+    'oasys1>=1.1.4',
     #'wofryshadow',
-    'shadow3>=18.1.24'
+    'shadow3>=18.4.11'
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
