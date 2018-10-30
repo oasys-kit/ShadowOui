b2003beb725ebaa6f74d7b6d46278443a7e45725
lucarebuffi <lrebuffi@anl.gov>
2018-10-30 17:02:34 -0500

    Full Undulator!

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.4.70'
+VERSION = '1.4.71'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'srxraylib>=1.0.19',
+    'srxraylib>=1.0.20',
     'oasys1>=1.1.13',
     'wofryshadow',
     'shadow3>=18.4.11'
