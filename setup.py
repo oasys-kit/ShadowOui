19893ddecf1bd1535cba610bc46724011fcd6937
lucarebuffi <lrebuffi@anl.gov>
2018-11-19 09:23:01 -0600

    Fixed Dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.4.76'
+VERSION = '1.4.77'
 ISRELEASED = False
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'shadow3>=18.4.11'
+    'shadow3>=18.4.11',
     'oasys1>=1.1.19',
     'wofryshadow',
 )
