c934d38946e8395c3ac9b8fa8e2ac1f543be8b5c
Manuel Sanchez del Rio <srio@esrf.eu>
2019-08-15 15:24:27 -0700

    raised new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.5.12'
+VERSION = '1.5.13'
 ISRELEASED = True
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
