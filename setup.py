6213f09cd61416e566474ace6eaf96665514b45e
lucarebuffi <lrebuffi@anl.gov>
2019-08-16 13:33:44 -0500

    New Features

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.5.13'
+VERSION = '1.5.14'
 ISRELEASED = True
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -41,7 +41,7 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
     'shadow3>=18.5.30',
-    'oasys1>=1.2.7',
+    'oasys1>=1.2.8',
     'wofryshadow>=1.0.2',
 )
 
