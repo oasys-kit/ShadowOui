b8f9657466fcb32f1795b234991c9ccbf87af87a
lucarebuffi <lrebuffi@anl.gov>
2020-08-11 15:45:34 -0500

    New Developments

---
+++ orangecontrib/shadow/util/zone_plates/FZP_Simulation_in_2D.py
@@ -0,0 +1,229 @@
+# #########################################################################
+# Copyright (c) 2020, UChicago Argonne, LLC. All rights reserved.         #
+#                                                                         #
+# Copyright 2020. UChicago Argonne, LLC. This software was produced       #
+# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
+# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
+# U.S. Department of Energy. The U.S. Government has rights to use,       #
+# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
+# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
+# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
+# modified to produce derivative works, such modified software should     #
+# be clearly marked, so as not to confuse it with the version available   #
+# from ANL.                                                               #
+#                                                                         #
+# Additionally, redistribution and use in source and binary forms, with   #
+# or without modification, are permitted provided that the following      #
+# conditions are met:                                                     #
+#                                                                         #
+#     * Redistributions of source code must retain the above copyright    #
+#       notice, this list of conditions and the following disclaimer.     #
+#                                                                         #
+#     * Redistributions in binary form must reproduce the above copyright #
+#       notice, this list of conditions and the following disclaimer in   #
+#       the documentation and/or other materials provided with the        #
+#       distribution.                                                     #
+#                                                                         #
+#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
+#       Laboratory, ANL, the U.S. Government, nor the names of its        #
+#       contributors may be used to endorse or promote products derived   #
+#       from this software without specific prior written permission.     #
+#                                                                         #
+# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
+# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
+# POSSIBILITY OF SUCH DAMAGE.                                             #
+# #########################################################################
+#%% Fresnel zone plate simulation code
+#%--------------------------------------------------------------------------
+#% by Joan Vila-Comamala from original IDL version of Ana Diaz (February, 2009)
+#% June, 2010
+#%
+#% code modified by Michael Wojcik Oct, 2013
+#%
+#% It simulates wavefront after FZP and propagates to the focal plane
+#% plots wavefield all through the propagation from the FZP to the focus.
+#%
+#% 2D Extension Code --> Wavefront propagation in made using the Hankel
+#% transform for a circularly symmetric function a long the radial
+#% coordinate. Hankel transform routine is Hankel_Transform_MGS.
+#%
+#% Keep your eyes open, this code has not been throughly debugged or tested!
+#%
+#%
+#%% ------------------------------------------------------------------------
+
+import numpy, xraylib
+from orangecontrib.shadow.util.shadow_util import ShadowPhysics
+from orangecontrib.shadow.util.zone_plates import bessel_zeros
+from orangecontrib.shadow.util.zone_plates.hankel_transform_MGS import Hankel_Transform_MGS
+## FZP, CS, OSA and simulation parameters
+#--------------------------------------------------------------------------
+  
+with_CS = 1     # equal to 0 --> without CS - CENTRAL STOP
+                # equal to 1 --> with CS
+               
+with_OSA = 0    # equal to 0 --> without OSA - ORDER SORTING APERTURE
+                # equal to 1 --> with OSA
+               
+FZP_TYPE = 0    # equal to 0 --> Ordinary FZP
+                # equal to 1 --> Zone-Doubled FZP
+                # equal to 2 --> Zone-Filled FZP
+                # equal to 3 --> Two-Level FZP
+                # equal to 4 --> Three-Level FZP
+                # equal to 5 --> ALD Multideposition FZP 1
+                # equal to 6 --> ALD Multideposition FZP 2
+                # equal to 7 --> Zone-Edge Slanted FZP
+              
+with_Range = 1  # equal to 0 --> plot to focal length
+                # equal to 1 --> plot in a given range
+               
+with_MultiSlicing = 0 # equal to 1 --> apply multisilcing of the element 
+                       # equal to 0 --> no multislicing
+                     
+with_Stacking = 0    # equal to 1 --> Two FZP are stacked
+                     # equal to 0 --> Single FZP
+                     # equal to 2 --> Three FZP are stacked
+                     
+with_Complex = 0  # equal to 0 --> Complex Wavefront is not stored
+                  # equal to 1 --> Complex Wavefront is storedk
+ 
+energy = 8                         # photon energy [keV]
+wavelength = 12.398/energy*1e-10   # wavelength [m]
+k = 2*numpy.pi/wavelength          # wavevector [m-1]
+height = 20000e-9              # zone thickness or height [m]
+height_2 = 750e-9              # zone thickness or height [m] of second zone plate
+height_3 = 400e-9              # zone thickness or height [m] of third zone plate
+diam = 50e-6                   # FZP diameter [m]
+bmin = 50e-9                   # outermost zone width [m] / outermost period for ZD [m]
+f = diam*bmin/wavelength             # focal distance [m]
+Distance_Stacking = 0e-6          # Separation distance between stacked zone plates
+Misalign = 0e-9                   # Misalignment of chosen zone plate in radial direction
+
+Range_i = (f-Distance_Stacking)-2e-6 # distance to FZP
+Range_f = (f-Distance_Stacking)+2e-6 # distance to FZP
+
+CS_diam = 10e-6                # beamstop diameter [m]
+OSA_position = 0.03          # distance FZP-OSA [m]
+OSA_diam = 30e-6              # OSA diameter [m]
+
+N = 5000                      # Number of sampling point in radial direction 
+#R = 2*diam                     # Radius of the simulation    
+R = diam                     # Radius of the simulation    
+step = R/N
+Nzero = numpy.floor(1.25*diam/2/R*N) # Parameter to speed up the Hankel Transform
+                                     # when the function has zeros for N > Nzero
+Nz = 3                         # Number of sampling points along the z axis
+factor_z = 1.6                 # Z axis range up to factor_z*f 
+
+ii = 0 + 1j
+
+#% FZP Material
+#--------------------------------------------------------------------------
+
+FZP_Material = 'Au'
+Template_Material = 'SiO2'
+
+density_FZP      = ShadowPhysics.getMaterialDensity(FZP_Material)
+density_Template = ShadowPhysics.getMaterialDensity(Template_Material)
+
+delta_FZP = (1 - xraylib.Refractive_Index_Re(FZP_Material, energy, density_FZP))  # Coefficients for FZP material
+beta_FZP  = xraylib.Refractive_Index_Im(FZP_Material, energy, density_FZP)
+
+delta_template = (1 - xraylib.Refractive_Index_Re(Template_Material, energy, density_Template)) # Coefficienct for template material
+beta_template =  xraylib.Refractive_Index_Im(Template_Material, energy, density_Template)
+
+if (with_MultiSlicing==1): NSlices = 100 # Number of slices of the element
+else: NSlices = 1
+
+width_coating = 20e-9                  # Width of the coated material for 
+                                        # for a zone-filled FZP.
+## Parameters for Stair Case Profiles 
+#  Currently parameters for maximum at 6.2 keV using Au as FZP material                                        
+                                       
+#Two-level profile
+height1 = (2/3)*1000e-9
+height2 = (4/3)*1000e-9
+
+## Contruction of the FZP profile
+#--------------------------------------------------------------------------
+
+Nzones=int(numpy.floor(1.0/4.0*(diam/bmin)))
+
+radia = numpy.sqrt(numpy.arange(0, Nzones+1)*wavelength*f + ((numpy.arange(0, Nzones+1)*wavelength)**2)/4)
+profile = numpy.full(N, 1 + 0j)
+profile[int(numpy.floor(radia[Nzones]/step)):N] = 0
+
+# Ordinary FZP
+if (FZP_TYPE==0):
+    for i in range (1, Nzones, 2):
+       position_i = int(numpy.floor(radia[i]/step))
+       position_f = int(numpy.floor(radia[i+1]/step))
+       profile[position_i:position_f] = numpy.exp(-ii*(-2*numpy.pi*delta_FZP/wavelength*height-ii*2*numpy.pi*beta_FZP/wavelength*height))
+
+    Membrane_Transmission = 1
+
+#% Propagation of the wavefield
+# The routine performing the 0th order Hankel tranform is based in a
+# algorithm that calculates the function at positision related to the zeros
+# of the 0th order Bessel function.
+#--------------------------------------------------------------------------
+
+# Loading the position of the zeros, as much position as N+1. The file
+# c.mat contains up to 200000 zeros of the 1st order Bessel function.
+c = bessel_zeros['c'][0, 0:N+1]
+
+# Definition of the position where the calculated input and transformated
+# funtions are evaluated. We define also the maximum frequency in the
+# angular domain.
+
+Q = c[N]/(2*numpy.pi*R)   # Maximum frequency
+r = c[0:N]*R/c[N]         # Radius vector
+q = c[0:N]/(2*numpy.pi*R) # Frequency vector
+
+## Recalculation of the position where the initial profile is defined.
+# Originally the profile is defined in position r0, that are linear for all
+# the values of position. Now we need to define the function in a new
+# coordinates that are by r. The next loop is interpolating the values of
+# the profile from the coordinates in r0 to the coordinates in r.
+# The output intensity profiles will be defined in r coordinates.
+#--------------------------------------------------------------------------
+
+r0 = numpy.arange(0, R, R/N)
+profile_h = numpy.full(N, 0 + 0j)
+
+for i in range(0, N-1):
+    profile_h[i] = profile[i]+(profile[i+1]-profile[i])/(r0[i+1]-r0[i])*(r[i]-r0[i])
+profile_h[N-1]=profile[N-1]
+
+## Calculatio of the first angular spectrum 
+#--------------------------------------------------------------------------
+map_int=numpy.zeros((NSlices+Nz, N))
+if (with_Complex==1): map_complex = numpy.zeros((NSlices+Nz, N))
+
+# The function 'Hankel_Transform_MGS' needs as input the field 'field0', 
+# the maximum radius R and the zeros of the 0th Bessel function. In case of
+# the inverse transformation from the angular domain back to spatial domain
+# the function needs as a input the angular specturm 'fun', the maximum
+# frequency Q and the zeros of the Bessel function.
+
+
+field0 = profile_h*Membrane_Transmission
+map_int[0, :] = numpy.dot(numpy.abs(field0), numpy.abs(field0))
+if (with_Complex==1): map_complex[0, :] = field0
+
+four0 = Hankel_Transform_MGS(field0, R, c)
+#time = etime(clock,time_i)
+#disp(['Estimated time for one point ' num2str(floor(time/3600)) 'h ' num2str(floor(rem(time,3600)/60)) 'min ' num2str(floor(rem(rem(time,3600),60))) 's'])
+
+field0 = profile_h
+
+#plot(field0)
---
+++ orangecontrib/shadow/util/zone_plates/efficiency_MGS.py
@@ -0,0 +1,110 @@
+# #########################################################################
+# Copyright (c) 2020, UChicago Argonne, LLC. All rights reserved.         #
+#                                                                         #
+# Copyright 2020. UChicago Argonne, LLC. This software was produced       #
+# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
+# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
+# U.S. Department of Energy. The U.S. Government has rights to use,       #
+# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
+# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
+# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
+# modified to produce derivative works, such modified software should     #
+# be clearly marked, so as not to confuse it with the version available   #
+# from ANL.                                                               #
+#                                                                         #
+# Additionally, redistribution and use in source and binary forms, with   #
+# or without modification, are permitted provided that the following      #
+# conditions are met:                                                     #
+#                                                                         #
+#     * Redistributions of source code must retain the above copyright    #
+#       notice, this list of conditions and the following disclaimer.     #
+#                                                                         #
+#     * Redistributions in binary form must reproduce the above copyright #
+#       notice, this list of conditions and the following disclaimer in   #
+#       the documentation and/or other materials provided with the        #
+#       distribution.                                                     #
+#                                                                         #
+#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
+#       Laboratory, ANL, the U.S. Government, nor the names of its        #
+#       contributors may be used to endorse or promote products derived   #
+#       from this software without specific prior written permission.     #
+#                                                                         #
+# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
+# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
+# POSSIBILITY OF SUCH DAMAGE.                                             #
+# #########################################################################
+#% Calculates the Diffraction Efficiency of FZP
+#% -------------------------------------------------------------------------
+#% by Joan Vila, July 2010
+#
+#% Data from FZP simulations in 2D has to be loaded in memory.
+#%
+#% F_POS --> The position of the focus in units of Nz step has to be given.
+#% N_points --> Number of points considered around the focal spot to
+#%              integrate the intensity. (default = N)
+#%
+#% The code computes the incoming intensity by taking 1 for every pixel of
+#% the structure. The intensity in focus is integrate considering the
+#% profile that is obtained during the simulation.
+#%
+#% 27.11.2011 -- The formula for I had was taking the wrong range for map_d
+#%               by one point. This has been corrected.
+#%
+#% 10.11.2010 -- The code was adapted to the output ot the codes that use
+#%               Hankel Transform based on Manuel's code.
+#%
+#% 24.10.2010 -- The previous version was very roughly calculating the area
+#%               under the intensity distribution with rough rectangle
+#%               approximation. The new version is using a trapezoidal
+#%               integration of MATLAB.
+#% 01.09.2010 -- Version 1.0
+#%
+#% -------------------------------------------------------------------------
+
+import numpy
+
+def efficiency_MGS(F_POS, map_d, profile_h, r, N, N_points=0):
+    if N_points <= 0: N_points = N
+
+    I_dens_0 = numpy.zeros(N)
+    I_dens_0[numpy.where(profile_h != 0)] = 1.0
+    I_0 = numpy.trapz(r, numpy.multiply(r, I_dens_0))
+    I = 2*numpy.pi*numpy.trapz(r[0:N_points+1], numpy.multiply(r[0:N_points+1], map_d[N-1:(N+N_points), F_POS]))
+
+    return numpy.divide(I, I_0)
+'''
+if nargin < 5 || isempty(N_points)
+   N_points = N;
+end
+
+% Calculation of the incoming intensity
+% -------------------------------------------------------------------------
+
+I_dens_0 (1:N) = 0;
+
+I_dens_0 (profile_h ~=0) = 1.0;
+
+I_0 = 2*pi*trapz(r,r.*I_dens_0);
+%I_0 = 5000;
+
+% Calculation of the intensity in focus
+% -------------------------------------------------------------------------
+
+%I = 2*pi*trapz(r(1:N_points),r(1:N_points).*transpose(map_d(N+1:(N+N_points),F_POS)));
+I = 2*pi*trapz(r(1:N_points+1),r(1:N_points+1).*transpose(map_d(N:(N+N_points),F_POS)));
+
+%I = 2*pi*trapz(r(1:N_points+1),r(1:N_points+1).*transpose(map_d(N+1:(N+1+N_points),F_POS)));
+
+DE = I/I_0;
+
+%I=map(2*N+1:2*N+N_points,F_POS).*(1:N_points)'
+'''
