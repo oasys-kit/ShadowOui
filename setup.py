50e9bfa079aa0f4c47c5676ae0627debebd3b8a2
lucarebuffi <lrebuffi@anl.gov>
2022-02-04 07:25:08 -0600

    Fixed 3d Plot animations bug and New Beamline Renderer

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.5.139'
+VERSION = '1.5.140'
 ISRELEASED = True
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -41,7 +41,7 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
     'shadow3>=18.5.30',
-    'oasys1>=1.2.89',
+    'oasys1>=1.2.93',
     'wofryshadow>=1.0.3',
 )
 
