36e07432e1cf3f0de96a38cb40608b42d6828211
lucarebuffi <lrebuffi@anl.gov>
2023-02-28 15:03:43 -0600

    syned upgrade

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.5.210'
+VERSION = '1.5.211'
 ISRELEASED = True
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -41,7 +41,7 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
     'shadow3>=22.8.25',
-    'oasys1>=1.2.108',
+    'oasys1>=1.2.123',
     'wofryshadow>=1.0.4',
     'dabax',
     'xoppylib',
