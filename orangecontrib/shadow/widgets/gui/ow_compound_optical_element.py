7cc3f4bbe4a3d6cb9146e7b7828e4d606e27ee37
lrebuffi <lrebuffi@anl.gov>
2023-09-07 17:00:50 -0500

    bug fixing

--- orangecontrib/shadow/widgets/gui/ow_compound_optical_element.py
+++ orangecontrib/shadow/widgets/gui/ow_compound_optical_element.py
@@ -8,7 +8,7 @@ from orangewidget import gui
 from orangewidget import widget
 from orangewidget.settings import Setting
 from oasys.widgets import gui as oasysgui
-from oasys.util.oasys_util import EmittingStream, TTYGrabber, TriggerIn
+from oasys.util.oasys_util import EmittingStream, TTYGrabber, TriggerIn, TriggerOut
 
 from orangecontrib.shadow.widgets.gui import ow_generic_element
 from orangecontrib.shadow.util.shadow_objects import ShadowPreProcessorData, ShadowBeam, ShadowCompoundOpticalElement
@@ -21,6 +21,7 @@ class CompoundOpticalElement(ow_generic_element.GenericElement):
     keywords = ["data", "file", "load", "read"]
 
     inputs = [("Input Beam", ShadowBeam, "setBeam"),
+              ("Trigger", TriggerOut, "sendNewBeam"),
               ("PreProcessor Data", ShadowPreProcessorData, "setPreProcessorData")]
 
     outputs = [{"name": "Beam",
@@ -94,9 +95,99 @@ class CompoundOpticalElement(ow_generic_element.GenericElement):
                      sendSelectedValue=False, orientation="horizontal")
 
 
+    def sendNewBeam(self, trigger):
+        try:
+            if ShadowCongruence.checkEmptyBeam(self.input_beam):
+                if ShadowCongruence.checkGoodBeam(self.input_beam):
+                    if trigger and trigger.new_object == True:
+                        if trigger.has_additional_parameter("variable_name"):
+                            variable_name = trigger.get_additional_parameter("variable_name").strip()
+                            variable_display_name = trigger.get_additional_parameter("variable_display_name").strip()
+                            variable_value = trigger.get_additional_parameter("variable_value")
+                            variable_um = trigger.get_additional_parameter("variable_um")
+
+                            def check_options(variable_name):
+                                pass
+                                '''
+                                if variable_name in ["mm_mirror_offset_x",
+                                                     "mm_mirror_rotation_x",
+                                                     "mm_mirror_offset_y",
+                                                     "mm_mirror_rotation_y",
+                                                     "mm_mirror_offset_z",
+                                                     "mm_mirror_rotation_z"]:
+                                    self.mirror_movement = 1
+                                    self.set_MirrorMovement()
+                                elif variable_name in ["sm_offset_x_mirr_ref_frame",
+                                                       "sm_offset_y_mirr_ref_frame",
+                                                       "sm_offset_z_mirr_ref_frame",
+                                                       "sm_rotation_around_x",
+                                                       "sm_rotation_around_y",
+                                                       "sm_rotation_around_z"]:
+                                    self.source_movement = 1
+                                    self.set_SourceMovement()
+                                elif variable_name == "mirror_orientation_angle_user_value":
+                                    self.mirror_orientation_angle = 4
+                                    self.mirror_orientation_angle_user()
+                                elif variable_name == "incidence_angle_deg":
+                                    self.calculate_incidence_angle_mrad()
+                                elif variable_name == "incidence_angle_mrad":
+                                    self.calculate_incidence_angle_deg()
+                                elif variable_name == "reflection_angle_deg":
+                                    self.calculate_reflection_angle_mrad()
+                                elif variable_name == "reflection_angle_mrad":
+                                    self.calculate_reflection_angle_deg()
+                                elif variable_name in ["object_side_focal_distance", "image_side_focal_distance"]:
+                                    self.surface_shape_parameters = 0
+                                    self.focii_and_continuation_plane = 1
+                                    self.set_IntExt_Parameters()
+                                elif variable_name == "user_defined_bragg_angle":
+                                    self.diffraction_calculation = 1
+                                    self.set_UserDefinedBraggAngle()
+                                    self.set_DiffractionCalculation()
+                                elif variable_name in ["slit_width_xaxis", "slit_height_zaxis"]:
+                                    self.aperturing = 1
+                                    self.set_Aperturing()
+                                elif variable_name in ["thickness"]:
+                                    self.absorption = 1
+                                    self.set_Absorption()
+                                 '''
+                            def check_number(x):
+                                try:    return float(x)
+                                except: return x
+
+                            if "," in variable_name:
+                                variable_names = variable_name.split(",")
+
+                                if isinstance(variable_value, str) and "," in variable_value:
+                                    variable_values = variable_value.split(",")
+                                    for variable_name, variable_value in zip(variable_names, variable_values):
+                                        setattr(self, variable_name.strip(), check_number(variable_value))
+                                        check_options(variable_name)
+                                else:
+                                    for variable_name in variable_names:
+                                        setattr(self, variable_name.strip(), check_number(variable_value))
+                                        check_options(variable_name)
+                            else:
+                                setattr(self, variable_name, check_number(variable_value))
+                                check_options(variable_name)
+
+                            self.input_beam.setScanningData(ShadowBeam.ScanningData(variable_name, variable_value, variable_display_name, variable_um))
+
+                        self.traceOpticalElement()
+                else:
+                    raise Exception("Input Beam with no good rays")
+            else:
+                raise Exception("Empty Input Beam")
+
+        except Exception as exception:
+            QtWidgets.QMessageBox.critical(self, "Error",
+                                       str(exception), QtWidgets.QMessageBox.Ok)
+
+            if self.IS_DEVELOP: raise exception
+
+
     def traceOpticalElement(self):
         try:
-            #self.error(self.error_id)
             self.setStatusMessage("")
             self.progressBarInit()
 
