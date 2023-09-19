2f56c0e18d60c5ca322e3d57c660e83667b090f0
Manuel Sanchez del Rio <srio@esrf.eu>
2023-09-19 10:41:21 +0200

    realign with new shadow3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.5.212'
+VERSION = '1.5.213'
 ISRELEASED = True
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -40,7 +40,7 @@ SETUP_REQUIRES = (
 )
 
 INSTALL_REQUIRES = (
-    'shadow3>=22.8.25',
+    'shadow3>=23.9.19',
     'oasys1>=1.2.123',
     'wofryshadow>=1.0.4',
     'dabax',
