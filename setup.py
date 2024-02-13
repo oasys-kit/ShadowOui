9c2b382e3273b1f762e9273cc06752d5a3d1e243
lrebuffi <lrebuffi@anl.gov>
2024-02-13 11:47:21 -0600

    new hybrid structure

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except AttributeError:
     from setuptools import find_packages, setup
 
 NAME = 'OASYS1-ShadowOui'
-VERSION = '1.5.222'
+VERSION = '1.5.223'
 ISRELEASED = True
 
 DESCRIPTION = 'Shadow, Ray-tracing simulation software'
@@ -41,12 +41,12 @@ SETUP_REQUIRES = (
 
 INSTALL_REQUIRES = (
     'shadow3>=23.9.19',
-    'oasys1>=1.2.131',
+    'oasys1>=1.2.141',
     'syned-gui>=1.0.1',
     'wofryshadow>=1.0.4',
     'dabax',
     'xoppylib>=1.0.21',
-    'oasys-hybrid-methods>=1.0.0',
+    'shadow-hybrid-methods>=1.0.0',
 )
 
 PACKAGES = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests'))
